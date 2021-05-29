be887f76700310badc4ef6b064b1a5e889834041
Anton Veldanov <anton_veldanov@intuit.com>
2021-05-28 20:42:55 -0700

    iOS

---
+++ ShopMeApp/StoreCollectionVC.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  ShopMeApp
+//
+//  Created by Anton Veldanov on 5/22/21.
+//
+
+import UIKit
+
+class StoreCollectionVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    
+    
+    
+    
+    
+    @IBAction func restoreButton(_ sender: UIButton) {
+    }
+    
+}
+
