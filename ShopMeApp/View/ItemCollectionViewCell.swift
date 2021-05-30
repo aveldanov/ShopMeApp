89087c38abdf2cf1626434b408208480992fc25e
Anton Veldanov <anton_veldanov@intuit.com>
2021-05-30 13:07:42 -0700

    iOS

---
+++ ShopMeApp/View/ItemCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ItemCollectionViewCell.swift
+//  ShopMeApp
+//
+//  Created by Anton Veldanov on 5/30/21.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    
+}
