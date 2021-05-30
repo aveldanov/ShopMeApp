4f1588fbd22c15386f1e7fd99324262ec0b6e997
Anton Veldanov <anton_veldanov@intuit.com>
2021-05-30 13:13:47 -0700

    iOS

--- ShopMeApp/View/ItemCollectionViewCell.swift
+++ ShopMeApp/View/ItemCollectionViewCell.swift
@@ -9,4 +9,20 @@ import UIKit
 
 class ItemCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var itemImageView: UIImageView!
+    
+    @IBOutlet weak var itemNameLabel: UILabel!
+    
+    @IBOutlet weak var itemPriceLabel: UILabel!
+    
+    
+    
+    func configure(forItem item: Item){
+        
+        itemImageView.image = item.image
+        itemNameLabel.text = item.name
+        itemPriceLabel.text = String(item.price)
+        
+    }
+    
 }
