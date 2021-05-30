0da42303e0a196a2f4fed82b481dcfd8c0108c0d
Anton Veldanov <anton_veldanov@intuit.com>
2021-05-30 12:52:16 -0700

    iOS

--- ShopMeApp/Model/Item.swift
+++ ShopMeApp/Model/Item.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+class Item {
+    public private(set) var image: UIImage
+    public private(set) var name: String
+    public private(set) var price: Double
+    
+    init(image: UIImage, name: String, price: Double) {
+        self.image = image
+        self.name = name
+        self.price = price
+    }
+}
