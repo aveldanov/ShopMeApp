1d9e556c3d9c68fa2caa49f0b9fe1b32e4d3dd68
Anton Veldanov <anton.veldanov@gmail.com>
2021-06-07 19:12:34 -0700

    iOS

---
+++ ShopMeApp/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  ShopMeApp
+//
+//  Created by Anton Veldanov on 6/7/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
