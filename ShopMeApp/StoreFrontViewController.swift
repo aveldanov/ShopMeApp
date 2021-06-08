5441c831ef5dd59d6899a644301df6e0c229e172
Anton Veldanov <anton.veldanov@gmail.com>
2021-06-07 18:47:59 -0700

    iOS

--- ShopMeApp/StoreFrontViewController.swift
+++ ShopMeApp/StoreFrontViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class StoreFrontViewController: UIViewController {
+class StoreFrontViewController: UIViewController,UICollectionViewDelegate, UICollectionViewDataSource {
 
+    
+
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        
+        
+        return UICollectionViewCell()
+    }
 
-
+    @IBAction func restoreButtonClicked(_ sender: UIButton) {
+    }
+    
 }
 
