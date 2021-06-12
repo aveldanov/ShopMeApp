1e1e5e79b11e4ab549c75f0a46174eff8cfcc4f5
Anton Veldanov <anton.veldanov@gmail.com>
2021-06-11 19:34:51 -0700

    iOS

--- ShopMeApp/Services/IAPService.swift
+++ ShopMeApp/Services/IAPService.swift
@@ -12,13 +12,26 @@ import StoreKit
 class IAPService: NSObject, SKProductsRequestDelegate {
 
     static let shared = IAPService()
-    
+    var productIds = Set<String>()
+    var productRequest = SKProductsRequest()
     
     func loadData(){
         // profuctIdToStringSet()
+        // requestProducts(productIds)
         
         
+    }
+    
+    func prodcutIdToStringSet(){
+        productIds.insert(IAP_ITEM_ID)
+    }
+    
+    func requestProducts(forId ids: Set<String>){
+        productRequest = SKProductsRequest(productIdentifiers: ids)
+        productRequest.delegate = self
         
+        //start the process
+        productRequest.start()
     }
     
     
