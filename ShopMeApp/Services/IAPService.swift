b516406c3cc65f0b274ee1620d9267071336e4c7
Anton Veldanov <anton.veldanov@gmail.com>
2021-06-11 19:35:31 -0700

    iOS

--- ShopMeApp/Services/IAPService.swift
+++ ShopMeApp/Services/IAPService.swift
@@ -27,10 +27,11 @@ class IAPService: NSObject, SKProductsRequestDelegate {
     }
     
     func requestProducts(forId ids: Set<String>){
+        productRequest.cancel() // cancel all ongoing product requests
         productRequest = SKProductsRequest(productIdentifiers: ids)
         productRequest.delegate = self
         
-        //start the process
+        //start the process to send request to App Store
         productRequest.start()
     }
     
